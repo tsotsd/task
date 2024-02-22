9c15337f8ee640166647ecf209bcf919471c9fab
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-02-22 22:17:56 +0800

    monthNumber

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-const user = {
-    name: 'Alex',
-    age: 25,
-    isAdmin: true,
-    'city of residence': 'Moscow'
+let monthNumber = 13;
+if (monthNumber < 13 && monthNumber > 0) { 
+switch (monthNumber) {
+    case 1:
+        console.log('winter');
+        break;
+    case 2:
+        console.log('winter');
+        break;
+    case 3:
+        console.log('spring');
+        break;
+    case 4:
+        console.log('spring');
+        break;
+    case 5:
+        console.log('spring');
+        break;
+    case 6:
+        console.log('summer');
+        break;
+    case 7:
+        console.log('summer');
+        break;
+    case 8:
+        console.log('summer');
+        break;
+    case 9:
+        console.log('autumn');
+        break;
+    case 10:
+        console.log('autumn');
+        break;
+    case 11:
+        console.log('autumn');
+        break;
+    case 12:
+        console.log('winter');
+        break;
+    default:
+        break;
 }
-
-
-user.age = 30;
-delete user['city of residence'];
-
-console.log(user);
+} else {
+console.log('номер месяца больше 12 или меньше 1');
+}

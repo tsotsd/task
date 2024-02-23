cd9209e9548e03e8fb926d39451020764364f9cb
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-02-23 11:40:00 +0800

    task 2_3_7

--- script.js
+++ script.js
@@ -1,45 +1,7 @@
-let monthNumber = 13;
-if (monthNumber < 13 && monthNumber > 0) { 
-switch (monthNumber) {
-    case 1:
-        console.log('winter');
-        break;
-    case 2:
-        console.log('winter');
-        break;
-    case 3:
-        console.log('spring');
-        break;
-    case 4:
-        console.log('spring');
-        break;
-    case 5:
-        console.log('spring');
-        break;
-    case 6:
-        console.log('summer');
-        break;
-    case 7:
-        console.log('summer');
-        break;
-    case 8:
-        console.log('summer');
-        break;
-    case 9:
-        console.log('autumn');
-        break;
-    case 10:
-        console.log('autumn');
-        break;
-    case 11:
-        console.log('autumn');
-        break;
-    case 12:
-        console.log('winter');
-        break;
-    default:
-        break;
-}
+let number = +prompt('Пожалуйста, введите любое число', '0');
+
+if (Number.isFinite(number)) {
+    number % 2 === 0 ? console.log('even') : console.log('odd');
 } else {
-console.log('номер месяца больше 12 или меньше 1');
+    console.log(NaN);
 }

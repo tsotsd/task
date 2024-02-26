d51757112a05092590367f32b41d5ddf464723a5
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-02-26 16:45:10 +0800

    mult arr

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+let a = ['a', 'b', 'c'];
+let b = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9], [10],
+];
+
+for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < b[i].length; j++) {
+        console.log(b[i][j]);
+    }
+}
+
+// for (const item of b) {
+//     for (const el of item) {
+//         console.log(el);
+//     }
+// }
+
+// for (let i = 0; i < b.length; i++) {
+//     for (let j = 0; j < b[i].length; j++) {
+//         console.log(b[i][j]);
+//     }
+// }
+
+// console.log(a);
+// console.log(b);
+
+// for (let i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+// for (let i = 0; i < b.length; i++) {
+//     let c = b[i];
+//     for (let k = 0; k < c.length; k++) {
+//         console.log(c[k]);
+//     }
+// }
+
+
+ 
+// const newspaper = {
+// 	sports : {
+//         title: 'ARod Hits Home Run',
+// 	    writes: ['Miramon Nuevo', 'Rick Reilly', 'Woddy Paige'],
+//     },
+//     business : {
+//         title: 'GE Stock Dips Again',
+// 	    writes: ['Adam Smith', 'Albert Humphrey', 'Charles Handy'],
+//     },
+//         movies : {
+//         title: 'Superman Is A Flop',
+// 	    writes: ['Rogert Ebert', 'Andrew Sarris', 'Wesley Morris'],
+//     },        
+// }
+
+// console.log(newspaper.length);
+
+// const book = [
+// 	{ name: 'foreword', pageCount: 14 },
+// 	{ name: 'boyWhoLived', pageCount: 18 },
+// 	{ name: 'vanishingGlass', pageCount: 13 },
+// 	{ name: 'lettersFromNoOne', pageCount: 17 },
+// 	{ name: 'afterword', pageCount: 19 },
+// ];
+
+// console.log(book[1].pageCount);
+
+
 // const arr = [3, 5, 6, 2, 9, 10];
 // const even = arr.filter(item => item % 2 == 0);
 // console.log(even);

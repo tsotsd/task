ce762e3ee3add58cf4c7d4a97b17b5cbccc475be
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-03-01 16:42:06 +0800

    task2_8(2_1)

--- task.js
+++ task.js
@@ -1,20 +1,51 @@
-function isPositive() {
-    // писать код тут
+
+
+
+
+function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  function map(arr, ruleFunction) {
+    const output = [];
+  
+    // Добавил консоль лог для отладки
+    console.log("ruleFunction", ruleFunction);
+  
+    for (let i = 0; i < arr.length; i++) {
+      output.push(ruleFunction(arr[i]));
     }
-    function isMale() {
-    // писать код тут
-    }
-    function filter() {
-    // писать код тут
-    }
-    
-    console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
-    
-    const people = [
-       {name: 'Глеб', gender: 'male'},
-       {name: 'Анна', gender: 'female'},
-       {name: 'Олег', gender: 'male'},
-       {name: 'Оксана', gender: 'female'}
-    ];
-    
-    console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
+  
+    return output;
+  }
+  
+  // Добавим функцию-обертку, которая «синхронизирует» функцию add с колбэком, который ожидает map-функция
+  console.log(map([1, 4, 9, 16], (item) => {return add(10, item);})); // => [ 11, 14, 19, 26 ]
+
+// // ------------------------------
+// // const people = [
+// //     { name: 'Глеб', age: 29 },
+// //     { name: 'Анна', age: 17 },
+// //     { name: 'Олег', age: 7 },
+// //     { name: 'Оксана', age: 47 }
+// //  ];
+ 
+//  // Допишите колбэк для sort, изучите, как работает колбэк, в документации
+//  console.log(people.sort((a, b) => {
+//     if (a.age > b.age) {
+//         return 1;
+        
+//     }
+//     if (a.age < b.age) {
+//         return -1;
+//     }
+
+//     return 0;
+//  }));
+//  // код выше должен вывеcти =>
+//  // [
+//  //  { name: 'Олег', age: 7 },
+//  //  { name: 'Анна', age: 17 },
+//  //  { name: 'Глеб', age: 29 },
+//  //  { name: 'Оксана', age: 47 }
+//  // ]

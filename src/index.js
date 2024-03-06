0f98b1b44316e3fb322bfc30285c29b0074028f4
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-03-06 11:04:07 +0800

    task 2_10_4 practice

--- src/index.js
+++ src/index.js
@@ -9,22 +9,30 @@ const colorInputElement = document.getElementById("color-input");
 //   bodyElement.addEventListener('click', () => {
 //     bodyElement.style.backgroundColor = 'red'
 //   })
-  
+
 // }
+const initEventListnersHi = () => {
+  const buttonsElementsHis = document.querySelectorAll(".hi");
 
-
+  for (const buttonElementHi of buttonsElementsHis) {
+    buttonElementHi.addEventListener("click", () => {
+      alert(`Hi ${buttonElementHi.dataset.name}`);
+    });
+  }
+};
 
 const initEventListners = () => {
   const studentElements = document.querySelectorAll(".student");
 
   for (const studentElement of studentElements) {
     studentElement.addEventListener("click", () => {
-      console.log(1);
+      console.log(studentElement.dataset.color);
     });
   }
 };
 
 initEventListners();
+initEventListnersHi();
 
 buttonElement.addEventListener("click", () => {
   nameInputElement.classList.remove("input-error");
@@ -36,14 +44,16 @@ buttonElement.addEventListener("click", () => {
 
   listElement.innerHTML =
     listElement.innerHTML +
-    `<li class="student">
+    `<li class="student" data-color="${colorInputElement.value}">
         <p class="student-name">
           ${nameInputElement.value}, любимый цвет
           <span style="color: ${colorInputElement.value}"> ${colorInputElement.value}</span>
         </p>
+        <button class="hi" data-name="${nameInputElement.value}">Приветствовать</button>
       </li>`;
 
   initEventListners();
+  initEventListnersHi();
 
   nameInputElement.value = "";
 });

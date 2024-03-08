55e7530d2ca9f70bbb274273590dae19006ce9e6
tsotsd <135599352+tsotsd@users.noreply.github.com>
2024-03-08 21:46:47 +0800

    Продолжение

--- script.js
+++ script.js
@@ -1,28 +1,113 @@
-// person — это объект
-const person = {
-    // name, family, age — это свойства, или ключи, объекта person
-    name: 'Анатолий',
-    age: 29,
-    // Объекты могут быть вложенными, в ключе family лежит объект со свойствами mother, father и children
-    family: {
-       mother: 'Елена',
-       father: 'Антон',
-       // В ключах объекта могут лежать массивы, в ключе children лежат дети Анатолия
-       children: ['Варвара', 'Леонид']
-   } 
- };
- 
- // Так мы получаем значение свойства объекта
- console.log(person.name); // => Анатолий
- console.log(person.family.father); // => Антон
- 
- // Так мы можем изменить значение свойства в объекте
- person.name = 'Иван'
- console.log(person.name); // => Иван
- 
- 
- // Так мы вызываем метод join у массива children
- console.log(person.family.children.join(' и ')); // => Варвара и Леонид
- 
- // Так мы подставляем значения в строку с помощью шаблонных строк
- console.log(`Имя: ${person.name}. Дети: ${person.family.children.join(' и ')}`); // => Имя: Иван. Дети: Варвара и Леонид
+"use strict";
+// Код писать здесь
+const buttonElement = document.getElementById('add-name-comment');
+const listElement = document.getElementById('list');
+const nameInputElement = document.getElementById('name-input');
+const commentTextareaElement = document.getElementById('comment-textarea');
+
+const comments = [
+  {
+    name: 'Глеб Фокин',
+    comment: 'Это будет первый комментарий на этой странице',
+    like: 3,
+  },
+  {
+    name: 'Варвара Н.',
+    comment: 'Мне нравится как оформлена эта страница! ❤',
+    like: 75,
+  },
+];
+
+
+const renderComments = () => {
+  
+}
+
+let myDate = new Date();
+
+let todayDate = myDate.getDate();
+let todayMonth = myDate.getMonth() + 1;
+if (todayDate < 10) {
+  todayDate = '0' + todayDate;
+}
+if (todayMonth < 10) {
+  todayMonth = '0' + todayMonth;
+}
+let hour = myDate.getHours();
+let minute = myDate.getMinutes();
+
+if (hour < 10) {
+  hour = '0' + hour;
+}
+if (minute < 10) {
+  minute = '0' + minute;
+}
+
+let dateHours = todayDate + '.' + todayMonth + '.' + myDate.getFullYear().toString().substr(-2) + ' ' + hour + ':' + minute;
+console.log(dateHours);
+console.log(buttonElement.disabled);
+
+const currentDate = new Date().toLocaleDateString('default', {day: '2-digit', month: '2-digit', year: '2-digit'}) + " "+ new Date().toLocaleTimeString().slice(0, -3);
+console.log(currentDate);
+
+  buttonElement.addEventListener('click', () => {
+  // nameInputElement.classList.remove('error');
+  // commentTextareaElement.classList.remove('error');
+
+  console.log(buttonElement.removeAttribute);
+
+  if (nameInputElement.value === '' || commentTextareaElement.value ==='') {
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    buttonElement.removeAttribute('disabled');
+  }
+
+  // if (commentTextareaElement.value === '') {
+  //   commentTextareaElement.classList.add('error');
+  //    return; 
+  // }
+
+  const oldListElement = listElement.innerHTML;
+  listElement.innerHTML = oldListElement + ` <li class="comment">
+      <div class="comment-header">
+        <div>${nameInputElement.value}</div>
+        <div>${dateHours}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text">
+          ${commentTextareaElement.value}
+        </div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">0</span>
+          <button class="like-button"></button>
+        </div>
+      </div>
+    </li>`
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.code === 'Enter') {
+      const oldListElement = listElement.innerHTML;
+  listElement.innerHTML = oldListElement + ` <li class="comment">
+      <div class="comment-header">
+        <div>${nameInputElement.value}</div>
+        <div>${dateHours}</div>
+      </div>
+      <div class="comment-body">
+        <div class="comment-text">
+          ${commentTextareaElement.value}
+        </div>
+      </div>
+      <div class="comment-footer">
+        <div class="likes">
+          <span class="likes-counter">0</span>
+          <button class="like-button"></button>
+        </div>
+      </div>
+    </li>`
+    }
+  })
+
+console.log("It works!");
